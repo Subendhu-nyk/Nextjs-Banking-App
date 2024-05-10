81a20bd74ba94238be2c452287ea9dadc54ad861
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-10 20:25:04 +0530

    dekstop sidebar completed

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,26 @@
+import MobileNav from "@/components/mobile-navbar";
+import Sidebar from "@/components/sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn={firstName:'Sam',lastName:'Jsm'}
   return (   
-        <main>
-            Sidebar
-           {children}
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn}/>
+            <div className="flex size-full flex-col">
+              <div className="root-layout">
+                <Image src='/icons/BankCardLogo.png' width={30} height={30} alt='menu-icon' />
+                <div >
+                <MobileNav user={loggedIn}/>
+                </div>
+              </div> 
+              {children}             
+            </div>
+           
         </main>   
   );
 }
